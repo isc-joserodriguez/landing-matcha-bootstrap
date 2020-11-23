35de5056fc97b620056254d14742a44b65a5aa3b
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-11-22 19:30:07 -0700

    Tools

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 window.onload = () => {
     let posY = window.pageYOffset || document.documentElement.scrollTop;
+    if(posY>80){
+        document.querySelector('header').classList.add('hdr-sm', 'hdr-dw');
+    }
     window.addEventListener('scroll', function (e) {
         var distanceY = window.pageYOffset || document.documentElement.scrollTop;
         if (distanceY > 40) {
@@ -10,8 +13,6 @@ window.onload = () => {
             }
         } else {
             document.querySelector('header').classList.remove('hdr-sm','hdr-dw','hdr-up');
-        }
-
-        posY = window.pageYOffset || document.documentElement.scrollTop;
+        }posY = window.pageYOffset || document.documentElement.scrollTop;
     });
 };
